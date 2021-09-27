eb0bc14badbae8406961de94fafd891ddc59be56
Wei <weihsu@inquartik.com>
2021-09-28 00:04:42 +0800

    feat: add metawearhter api

---
+++ src/apis/metaweather.js
@@ -0,0 +1,21 @@
+const BASE_URL = 'https://www.metaweather.com/api/location'
+
+export async function getLocations({ query }) {
+  if (query) {
+    const response = await fetch(`${BASE_URL}/search/?query=${query}`);
+    const data = await response.json()
+    return data;
+  }
+
+  return [];
+}
+
+export async function getLocation({ woeid }) {
+  if (woeid > -1) {
+    const response = await fetch(`${BASE_URL}/${woeid}`);
+    const data = await response.json()
+    return data;
+  }
+
+  return null;
+}
