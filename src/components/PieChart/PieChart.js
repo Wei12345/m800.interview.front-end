86577bcbc701f6c4cc4367dc4dd1a5b0f71610e9
Wei <weihsu@inquartik.com>
2021-09-28 00:13:13 +0800

    feat: add pie-chart component

---
+++ src/components/PieChart/PieChart.js
@@ -0,0 +1,23 @@
+import styles from './PieChart.module.scss';
+
+export default function PieChart({ className, percentage }) {
+  const PI = 3.1415926;
+
+  const SIZE = 64;
+  const radius = SIZE / 2;
+  const circumference = radius * 2 * PI;
+  const percentageLength = percentage * circumference / 100;
+
+  return (
+    <svg className={`${styles.PieChart} ${className}`} viewBox={`0 0 ${SIZE} ${SIZE}`}>
+      <circle
+        r="50%"
+        cx="50%"
+        cy="50%"
+        stroke-dasharray={`${percentageLength} ${circumference}`}
+        className={styles.PieChart__Circle}
+      />
+      <text x="50%" y="50%" className={styles.PieChart__Text} >{percentage}%</text>
+    </svg>
+  )
+}
